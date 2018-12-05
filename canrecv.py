9228552407447c8e6d944dfd4b095068c25a0c03
PygmyGecko <pygmygcko@gmail.com>
2018-12-05 18:38:06 +0000

    Implemented MPU6050 and improved log file formatting

--- canrecv.py
+++ canrecv.py
@@ -5,26 +5,38 @@ from time import time
 from mpu6050 import mpu6050
 import sys, os
 
+
 timestamp = time()
+
 
 try:
 	dev = cantact.CantactDev("/dev/ttyACM0") # Connect to CANable
 except Exception:
-	print(device + " not found")
+	print("CAN device not found")
 	exit()
 
 dev.set_bitrate(500000) # Set the bitrate to a 500k baud
 dev.start() # Go on the bus
+
+
 file = open('./logs/log' + str(len(os.listdir('./logs'))), 'w+') # Create log file to write to
 
+
 sensor = mpu6050(0x68)
-accelerometor_data = sensor.get_accel_data()
+
 
 count = 0
 
 while True:
     count += 1
-    frame = dev.recv() # Receive a CAN frame
-    outputString = str(count) + "\t" + str(time() - timestamp) + "\t" + str(frame.id) + "\t" + accelerometor_data
-    print outputString  # Print out the received frame
+    
+    #frame = dev.recv() # Receive a CAN frame
+    
+    outputString = (str(count) + "\t" + str("{0:.3f}".format(time() - timestamp))
+    + "\t" + "dev.recv().id"  #Print Arbitration ID of CAN msg
+    + "\t" + str("{0:.3f}".format(sensor.get_accel_data()['x']))  #Print accel x value
+    + "\t" +  str("{0:.3f}".format(sensor.get_accel_data()['y']))  #Print accel y value 
+    + "\t" +  str("{0:.3f}".format(sensor.get_accel_data()['z'])))   #Print accel z value
+
+    print(outputString)  # Print out the received frame
     file.write(outputString + "\n") # Write received frame to log file
